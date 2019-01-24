fe07052c3f6408d5bdc8a43f2d489aebaa35e201
AndreevSemen <semen.andreev00@mail.ru>
2019-01-24 23:59:19 +0300

    New repos. try

--- source/main.cpp
+++ source/main.cpp
@@ -1,72 +1,105 @@
-// Copyright 2018 RomAsya
+// Copyright 2019 AndreevSemen
+#include <sstream>
 #include <Request.hpp>
 #include <Response.hpp>
 #include <Skill.hpp>
+#include <decryption.hpp>
+#include <encryption.hpp>
 
-void my_awesome_callback(const Alice::Request& request,
-                         Alice::Response& response) {
-  if (request.Command() == "Hello") {
-    Alice::Button first_button("Print on button", {}, "http://example.com/",
-                               true);
-    Alice::Button second_button("Print on button", {}, "http://example.com/",
-                                true);
-    response.PushButton(first_button);
-    response.PushButton(second_button);
-    Alice::ButtonPicture button_picture("Print on button",
-                                        "http://example.com/", {});
-    Alice::Card card("BigImage", "1027858/46r960da47f60207e924",
-                     "Title for image", "Description of image", button_picture);
-    response.SetCard(card);
-    response.SetText("Hi, dear friend!");
-    response.SetTts("Hi, dear friend!");
-    response.SetEndSession(false);
-  }
-  return;
-}
+std::string GetPrefix(const std::string&);
+__uint32_t GetKey(const std::string&);
+std::string GetText(const std::string&);
 
-void buy_elephant_callback(const Alice::Request& request,
-                           Alice::Response& response) {
-  /*const auto& session = request.Session();
-  if (session.IsNew() == true) {
-    response.SetText("Hi, buy an elephant!");
-    response.SetTts("Hi, buy an elephant!");
-    Alice::Button button(
-        "Ok", {},
-        "https://yandex.ru/images/"
-        "search?pos=1&img_url=https%3A%2F%2Fsummerboard.ru%2Fimages%"
-        "2Felephants_wallpapers%2F39_elephant.jpg&text=%D1%84%D0%BE%D1%82%D0%"
-        "BE%20%D1%81%D0%BB%D0%BE%D0%BD%D0%B0&lr=213&rpt=simage",
-        true);
-    response.PushButton(button);
-    response.SetEndSession(false);
-    return;
-  }*/
-  if (request.RequestType() == Alice::Request::Type::SimpleUtterance) {
-    std::string title;
-    if (request.Command() == "") {
-      title = "Купи слона!";
+void CryptoCallback(const Alice::Request& request,
+                    Alice::Response& response)
+{
+    if (request.RequestType() == Alice::Request::Type::SimpleUtterance)
+    {
+        std::string title;
+        if (request.Command() == "")
+        {
+            title = "Привет!\n"
+                    "Чтобы зашифровать текст, введите сначала ключ шифрования "
+                    "(он должен занимать ровно 8 первых символов текста), а затем текст.\n"
+                    "Чтобы расшифровать ранее зашифрованный текст, введите ключ, "
+                    "аналогично, как для шифрования, и выданый при шифровании текст.";
+            Alice::Button endingSessionButton("Пока!", {"end"}, "", false);
+        } else {
+            title = "Выберете действие с помощью соответсвующей кнопки";
+            Alice::Button encryptionButton("Шифруй", {"encry " + request.Command()}, "", true);
+            Alice::Button decryptionButton("Расшифровывай", {"decry " + request.Command()}, "", true);
+            response.PushButton(encryptionButton);
+            response.PushButton(decryptionButton);
+        }
+        response.SetText(title);
     } else {
-      title =
-          "Все говорят " + request.Command() + ", а ты купи слона!";
+        if (request.OriginalUtterance() == "end")
+        {
+            response.SetEndSession(true);
+        } else {
+            std::string prefix = GetPrefix(request.OriginalUtterance());
+            __uint32_t key = GetKey(request.OriginalUtterance());
+            std::string text = GetText(request.OriginalUtterance());
+            if (prefix == "encry")
+            {
+                Encryption(text, key);
+            } else if (prefix == "decry") {
+                Decryption(text, key);
+            }
+            response.SetText(text);
+        }
     }
-    response.SetText(title);
-    response.SetTts(title);
-    Alice::Button button("Я сдаюсь", {"json"}, true);
-    response.PushButton(button);
-  } else {
-    Alice::ButtonPicture button_picture(
-        "",
-        "http://pngimg.com/uploads/elephants/elephants_PNG18808.png", {"json"});
-    Alice::Card card("BigImage", "213044/0bd9aa9bce3a08478953", "", "",
-                     button_picture);
-    response.SetCard(card);
-    response.SetEndSession(true);
-  }
 }
 
-int main() {
-  Skill s;
-  s.SetCallback(buy_elephant_callback);
-  s.Run();
-  return 0;
+int main()
+{
+    Skill s;
+    s.setCallback(CryptoCallback);
+    s.run();
+    return 0;
 }
+
+std::string GetPrefix(const std::string& json)
+{
+    std::string prefix;
+    for (size_t i = 0; json[i] != ' '; ++i)
+    {
+        prefix += json[i];
+    }
+    return prefix;
+}
+
+__uint32_t GetKey(const std::string& json)
+{
+    size_t index = 0;
+    while (json[index] != ' ')
+    {
+        ++index;
+    }
+    ++index;
+    std::stringstream stream;
+    for (size_t i = 0; i < 8; ++index, ++i)
+    {
+        stream << std::hex << json[index];
+    }
+    __uint32_t key = 0;
+    stream >> key;
+    return key;
+}
+
+std::string GetText(const std::string& json)
+{
+    std::string text;
+    size_t index = 0;
+    while (json[index] != ' ')
+    {
+        ++index;
+    }
+    for (size_t i = 0; i < 8; ++index, ++i);
+    while (json[index])
+    {
+        text += json[index];
+    }
+    return text;
+}
+
