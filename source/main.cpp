a45479cd7aa6aa568203f689551f6375597a6ac5
AndreevSemen <semen.andreev00@mail.ru>
2019-01-25 22:10:35 +0300

    Remaked problem with symbols

--- source/main.cpp
+++ source/main.cpp
@@ -93,7 +93,7 @@ std::string GetText(const std::string& json)
     for (;json[index] != ' '; ++index);
     ++index;
     for (size_t i = 0; i < 8; ++index, ++i);
-    for (;json[index] != 0; ++index)
+    for (;json.size() - 2; ++index)
     {
         if (json[index] != '"' && json[index] != ']')
         {
