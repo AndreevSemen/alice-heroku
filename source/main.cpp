c52b5581b96c90e8b38b57ff6d6db4d2f30ebd71
Andreev Semen <semen.andreev00@mail.ru>
2019-01-28 13:31:10 +0300

    Added exception block

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,6 @@
 // Copyright 2019 AndreevSemen
 #include <sstream>
+#include <exception>
 #include <Request.hpp>
 #include <Response.hpp>
 #include <Skill.hpp>
@@ -13,47 +14,50 @@ std::string GetText(const std::string&);
 void CryptoCallback(const Alice::Request& request,
                           Alice::Response& response)
 {
-    if (request.RequestType() == Alice::Request::Type::SimpleUtterance)
-    {
-        std::string title;
-        if (request.Command() == "")
-        {
-            title = "Привет!\n"
-                    "Чтобы зашифровать текст, введите сначала ключ шифрования "
-                    "(он должен занимать ровно 8 первых символов текста), а затем текст.\n"
-                    "Чтобы расшифровать ранее зашифрованный текст, введите ключ, "
-                    "аналогично, как для шифрования, и выданый при шифровании текст.";
-            Alice::Button endingSessionButton("Пока!", {"end"}, false);
-            response.PushButton(endingSessionButton);
-        } else {
-            title = "Выберете действие с помощью соответсвующей кнопки";
-            Alice::Button encryptionButton("Шифруй",
-                                           {"encry " + request.Command()},
-                                           true);
-            Alice::Button decryptionButton("Расшифровывай",
-                                           {"decry " + request.Command()},
-                                           true);
-            response.PushButton(encryptionButton);
-            response.PushButton(decryptionButton);
-        }
-        response.SetText(title);
-    } else {
-        if (request.Payload().dump() == "end")
-        {
-            response.SetEndSession(true);
-        } else {
-            std::string json = request.Payload().dump();
-            std::string prefix = GetPrefix(json);
-            __uint32_t key = GetKey(json);
-            std::string text = GetText(json);
-            if (prefix == "encry")
-            {
-                text = Encryption(text, key);
-            } else if (prefix == "decry") {
-                text = Decryption(text, key);
+    try {
+        if (request.RequestType() == Alice::Request::Type::SimpleUtterance) {
+            std::string title;
+            if (request.Command() == "") {
+                title = "Привет!\n"
+                        "Чтобы зашифровать текст, введите сначала ключ шифрования "
+                        "(он должен занимать ровно 8 первых символов текста), а затем текст.\n"
+                        "Чтобы расшифровать ранее зашифрованный текст, введите ключ, "
+                        "аналогично, как для шифрования, и выданый при шифровании текст.";
+                Alice::Button endingSessionButton("Пока!", {"end"}, false);
+                response.PushButton(endingSessionButton);
+            } else {
+                title = "Выберете действие с помощью соответсвующей кнопки";
+                Alice::Button encryptionButton("Шифруй",
+                                               {"encry " + request.Command()},
+                                               true);
+                Alice::Button decryptionButton("Расшифровывай",
+                                               {"decry " + request.Command()},
+                                               true);
+                response.PushButton(encryptionButton);
+                response.PushButton(decryptionButton);
             }
-            response.SetText(text);
+            response.SetText(title);
+        } else {
+            if (request.Payload().dump() == "end") {
+                response.SetEndSession(true);
+            } else {
+                std::string json = request.Payload().dump();
+                std::string prefix = GetPrefix(json);
+                __uint32_t key = GetKey(json);
+                std::string text = GetText(json);
+                if (prefix == "encry") {
+                    text = Encryption(text, key);
+                } else if (prefix == "decry") {
+                    text = Decryption(text, key);
+                }
+                response.SetText(text);
+            }
         }
+    }
+    catch (std::exception& e)
+    {
+        std::string title = "Thrown: " + e.what();
+        response.SetText(title);
     }
 }
 
